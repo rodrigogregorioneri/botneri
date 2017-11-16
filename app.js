32bd5df3d50d072340fcef2841b23ca5b8f0c689
Rodrigo Neri <rodrigogregorioneri@hotmail.com>
2017-11-16 18:45:23 -0200

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var builder = require('botbuilder');
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
+
    console.log('%s listening to %s', server.name, server.url); 
 });
 
@@ -18,28 +19,10 @@ server.post('/api/messages', connector.listen());
 
 var bot = new builder.UniversalBot(connector, function (session) {
     session.send("CronApp: Posso ajudar em algo?");
-   
-   
-    session.send("CronApp: Olá não hetero");
-
-
-
-
-
-   
-   
-   
-   
 
 });
-   bot.dialog('nome', [
-    function (session) {
-        builder.Prompts.text(session, 'Olá, Qual seu nome??');
-    },
-    function (session, results) {
-        session.endDialogWithResult(results);
-    }
-]);
+
+
 
 bot.dialog('CronApp', [
     function (session) {
@@ -49,6 +32,8 @@ bot.dialog('CronApp', [
         session.endDialog(`Olá ${results.response}!`);
     }
 ]);
+
+
 
 bot.on('conversationUpdate', function (message) {
     if (message.membersAdded && message.membersAdded.length > 0) {
@@ -74,3 +59,44 @@ bot.on('conversationUpdate', function (message) {
         }
     }
 });
+
+// This bot ensures user's profile is up to date.
+var bot = new builder.UniversalBot(connector, [
+    function (session) {
+        session.beginDialog('ensureProfile', session.userData.profile);
+    },
+    function (session, results) {
+        session.userData.profile = results.response; // Save user profile.
+        session.send(`Olá ${session.userData.profile.name}! eu amo a ${session.userData.profile.company}!`);
+    }
+]);
+
+bot.dialog('ensureProfile', [
+    function (session, args, next) {
+        session.dialogData.profile = args || {}; // Set the profile or create the object.
+        if (!session.dialogData.profile.name) {
+            builder.Prompts.text(session, "Qual o seu nome?");
+        } else {
+            next(); // Skip if we already have this info.
+        }
+    },
+    function (session, results, next) {
+        if (results.response) {
+            // Save user's name if we asked for it.
+            session.dialogData.profile.name = results.response;
+        }
+        if (!session.dialogData.profile.company) {
+            builder.Prompts.text(session, "Aonde você trabalha?");
+        } else {
+            next(); // Skip if we already have this info.
+        }
+    },
+    function (session, results) {
+        if (results.response) {
+            // Save company name if we asked for it.
+            session.dialogData.profile.company = results.response;
+        }
+        session.endDialogWithResult({ response: session.dialogData.profile });
+    }
+]);
+
