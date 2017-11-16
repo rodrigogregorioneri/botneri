356951f2bf250b0bff78adfa617325c78db9ef27
Rodrigo Neri <rodrigogregorioneri@hotmail.com>
2017-11-16 18:46:49 -0200

    Update app.js

--- app.js
+++ app.js
@@ -17,48 +17,7 @@ var connector = new builder.ChatConnector({
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("CronApp: Posso ajudar em algo?");
 
-});
-
-
-
-bot.dialog('CronApp', [
-    function (session) {
-        builder.Prompts.text(session, 'Olá! Qual seu nome?');
-    },
-    function (session, results) {
-        session.endDialog(`Olá ${results.response}!`);
-    }
-]);
-
-
-
-bot.on('conversationUpdate', function (message) {
-    if (message.membersAdded && message.membersAdded.length > 0) {
-        // Say hello
-        var isGroup = message.address.conversation.isGroup;
-        var txt = isGroup ? "Posso ajudar em algo?" : "Bem vindo ao grupo CronApp, qualquer duvida estamos a disposição!!  ";
-        var reply = new builder.Message()
-                .address(message.address)
-                .text(txt);
-        bot.send(reply);
-    } else if (message.membersRemoved) {
-        // See if bot was removed
-        var botId = message.address.bot.id;
-        for (var i = 0; i < message.membersRemoved.length; i++) {
-            if (message.membersRemoved[i].id === botId) {
-                // Say goodbye
-                var reply = new builder.Message()
-                        .address(message.address)
-                        .text("Que pena esperamos que você volte para o CronApp!");
-                bot.send(reply);
-                break;
-            }
-        }
-    }
-});
 
 // This bot ensures user's profile is up to date.
 var bot = new builder.UniversalBot(connector, [
