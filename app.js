9342cc99801ac5ddacf7fffa8b65c7489531a0c9
Rodrigo Neri <rodrigogregorioneri@hotmail.com>
2017-11-17 15:09:50 -0200

    Update app.js

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    ppId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: '1f9ed36f-309d-4fef-afe8-401276d643a4',
+    appPassword: 'cvhbcCHHE963:+])qgUFP34'
 });
 
 
