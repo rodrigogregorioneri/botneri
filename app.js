78f3928a17d40fce8ead747bd99bc353e609a1bd
Rodrigo Neri <rodrigogregorioneri@hotmail.com>
2017-11-17 10:08:54 -0200

    Update app.js

--- app.js
+++ app.js
@@ -19,58 +19,15 @@ server.post('/api/messages', connector.listen());
 
 var userStore = [];
 var bot = new builder.UniversalBot(connector, function (session) {
-    // store user's address
-    var address = session.message.address;
-    userStore.push(address);
+   
+  
 
-    // end current dialog
-    session.endDialog('Você foi convidado para uma pesquisa! Começará em alguns segundos ...');
 });
 
-// Every 5 seconds, check for new registered users and start a new dialog
-setInterval(function () {
-    var newAddresses = userStore.splice(0);
-    newAddresses.forEach(function (address) {
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("O blockly é o futuro!!");
 
-        console.log('Starting survey for address:', address);
-
-        // new conversation address, copy without conversationId
-        var newConversationAddress = Object.assign({}, address);
-        delete newConversationAddress.conversation;
-
-        // start survey dialog
-        bot.beginDialog(newConversationAddress, 'survey', null, function (err) {
-            if (err) {
-                // error ocurred while starting new conversation. Channel not supported?
-                bot.send(new builder.Message()
-                    .text('This channel does not support this operation: ' + err.message)
-                    .address(address));
-            }
-        });
-
-    });
-}, 5000);
-
-bot.dialog('survey', [
-    function (session) {
-           builder.Prompts.text(session, 'Olá... Qual seu nome?');
-    },
-    function (session, results) {
-        session.userData.name = results.response;
-        builder.Prompts.number(session, 'Hi ' + results.response + ', How many years have you been coding?');
-    },
-    function (session, results) {
-        session.userData.coding = results.response;
-        builder.Prompts.choice(session, 'What language do you code Node using? ', ['JavaScript', 'CoffeeScript', 'TypeScript']);
-    },
-    function (session, results) {
-        session.userData.language = results.response.entity;
-        session.endDialog('Got it... ' + session.userData.name +
-            ' you\'ve been programming for ' + session.userData.coding +
-            ' years and use ' + session.userData.language + '.');
-    }
-]);
-
+});
 
 
 
