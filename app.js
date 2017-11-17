6534e1ec4511d117239d30c3a003389d79a3ee34
Rodrigo Neri <rodrigogregorioneri@hotmail.com>
2017-11-17 14:31:51 -0200

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var builder = require('botbuilder');
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
 
-   console.log('%s listening to %s', server.name, server.url); 
+    console.log('%s listening to %s', server.name, server.url);
 });
 
 // Create chat connector for communicating with the Bot Framework Service
@@ -14,29 +14,56 @@ var connector = new builder.ChatConnector({
     appPassword: 'cvhbcCHHE963:+])qgUFP34'
 });
 
+
+var menuItems = { 
+    "futuro": {
+        item: "futuro"
+    },
+    "produtividade": {
+        item: "produtividade"
+    },
+}
+
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
 // This is a reservation bot that has a menu of offerings.
 var bot = new builder.UniversalBot(connector, [
-    function(session, args){
-        session.send("Bem vindo ao CronApp.");
-        builder.Prompts.text(session, "Escolha umas das opções do menu \n - Descubra o futuro 'futuro' \n - Descubra a produtividade 'produtividade'");
- 
-    }, function(session, results){
-       resposta = results.response;
-       if(resposta === "futuro"){
-          session.send("O blockly é o futuro pois 'programadores são pagos pra encontrar solução'")
-       }else if(resposta === "produtividade"){
-          session.send("Com o Blockly você terá mais produtividade!!")      
-       }else{
-          session.send("Comando Invalido!!")
-       }   
+    function(session){
+        session.send("Bem vindo ao CronApp \n digite 'futuro'  \n digite 'produtividade'.");
+        session.beginDialog("mainMenu");
+    }
+]);
+
+bot.dialog("mainMenu", [
+    function(session){
+        builder.Prompts.choice(session, "Main Menu:", menuItems);
+    },
+    function(session, results){
+        if(results.response){
+            session.beginDialog(menuItems[results.response.entity].item);
+        }
+    }
+])
+.triggerAction({
+    // The user can request this at any time.
+    // Once triggered, it clears the stack and prompts the main menu again.
+    matches: /^main menu$/i,
+    confirmPrompt: "This will cancel your request. Are you sure?"
+});
+
+
+bot.dialog('futuro', [
+    function(session){
+        session.send("O futuro é o Blockly!");
+
     }
 ]);
 
 
+bot.dialog('produtividade', [
+    function(session){
+        session.send("Alta produtividade com o blockly!!!");
 
-
-
-
+    }
+]);
