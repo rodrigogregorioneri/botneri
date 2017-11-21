dfe710b4c8eecf0cf3382e2d46ac0c0c575de35d
Rodrigo Neri <rodrigogregorioneri@hotmail.com>
2017-11-21 11:51:39 -0200

    Update app.js

--- app.js
+++ app.js
@@ -35,16 +35,16 @@ var bot = new builder.UniversalBot(connector, [
        
         session.send("  Olá CronApp Users"); 
   
-      builder.Prompts.text(session, "\n  Para obter ajuda digite '@Cronappinho help'");
+      builder.Prompts.text(session, "\n  Para obter ajuda digite '@Cronappinho help' ou difite seu comando caso já saiba");
     },
     function (session, results) {
          // console.log('"Bem vindo ao Suporte CronApp. Em breve você terá autonomia para registrar seus chamados em nosso portal cronapp.io/suporte"., nosso horário de atendimento é de Seg. a Sex. das 9:00 às 18:00. Aguarde, um de nossos analistas responderá em breve.');
           if(results.response == "help"){
               session.send("Comandos de Ajuda  \n  - Ajuda : help  \n  - Horario de funcionamento: horario-funcionamento  \n  - Templates de aberturas de chamados: templates  \n  Contato comercial: comercial");
           }else if(results.response == "horario-funcionamento"){
-              session.send("Horário de atendimento é de Seg. a Sex. das 9:00 às 18:00. Aguarde, um de nossos analistas responderá em breve.");
+              session.send("Horário de atendimento é de Seg. a Sex. das 9:00 às 18:00.");
           }else if(results.response == "templates"){
-              session.send(" Projeto(s):  \n  Ambiente(s):  \n  Função do erro:  \n  \n  Passos:  \n  \n  OBS.:");
+              session.send("  Projeto(s):  \n  Ambiente(s):  \n  Função do erro:  \n  \n  Passos:  \n  \n  OBS.:");
           }else if(results.response == "comercial"){
               session.send("Comercial  \n  Representante: Gabriela Saeger  \n  Skype: Gabriela Saeger  \n Telefone: +55 00 99999-9999");
            }
