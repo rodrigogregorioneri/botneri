75c8fc023651c126a1f0f2349bedf5709f3fb9d5
Rodrigo Neri <rodrigogregorioneri@hotmail.com>
2017-11-21 12:07:10 -0200

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ var bot = new builder.UniversalBot(connector, [
     function (session, results) {
          // console.log('"Bem vindo ao Suporte CronApp. Em breve você terá autonomia para registrar seus chamados em nosso portal cronapp.io/suporte"., nosso horário de atendimento é de Seg. a Sex. das 9:00 às 18:00. Aguarde, um de nossos analistas responderá em breve.');
           if(results.response == "help"){
-              session.send("Comandos de Ajuda  \n  - Ajuda : help  \n  - Horario de funcionamento: horario-funcionamento  \n  - Templates de aberturas de chamados: templates  \n  - Contato comercial: comercial  \n  - Canal do Youtube: youtube  \n  - Webnars: webnar");
+              session.send("Comandos de Ajuda  \n  - Ajuda : help  \n  - Horario de funcionamento: horario-funcionamento  \n  - Templates de aberturas de chamados: templates  \n  - Contato comercial: comercial  \n  - Canal do Youtube: youtube  \n  - Webnars: webnars");
           }else if(results.response == "horario-funcionamento"){
               session.send("Horário de atendimento é de Seg. a Sex. das 9:00 às 13:00 horas e das 14:00 às 18:00 horas.");
           }else if(results.response == "templates"){
