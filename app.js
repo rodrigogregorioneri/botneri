230c1463d7086c639e0d6eab51d3d83baab08342
Rodrigo Neri <rodrigogregorioneri@hotmail.com>
2017-11-21 13:03:44 -0200

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ var connector = new builder.ChatConnector({
 });
 
 
-
 var menuItems = { 
     "help": {
         item: "help"
@@ -50,27 +49,7 @@ var bot = new builder.UniversalBot(connector, [
       session.send("Olá CronApp Users"); 
       session.beginDialog("mainMenu");
     }
-    // ,
-    // function (session, results) {
-    //      // console.log('"Bem vindo ao Suporte CronApp. Em breve você terá autonomia para registrar seus chamados em nosso portal cronapp.io/suporte"., nosso horário de atendimento é de Seg. a Sex. das 9:00 às 18:00. Aguarde, um de nossos analistas responderá em breve.');
-    //       if(results.response == "help"){
-    //           session.beginDialog("help");
-    //       }else if(results.response == "horario-funcionamento"){
-    //           session.send("Horário de atendimento é de Seg. a Sex. das 9:00 às 18:00.");
-    //       }else if(results.response == "templates"){
-    //           session.send("  Projeto(s):  \n  Ambiente(s):  \n  Função do erro:  \n  \n  Passos:  \n  \n  OBS.:");
-    //       }else if(results.response == "comercial"){
-    //           session.send("  Comercial  \n  Representante: Gabriela Saeger  \n  Skype: Gabriela Saeger  \n  Telefone: +55 00 99999-9999");
-    //        }else if(results.response == "youtube"){
-    //           session.send("Siga nosso canal no Youtube e fique por dentro dos nossos tutoriais e as gravações dos nossos Webnars  \n  youtube: 'https://www.youtube.com/channel/UCfEBjfXaiA8n-YfezFklsfg'")
-    //        }else if(results.response == "webnars"){
-    //           session.send("Cadastre-se nos webnars do CronApp: 'https://www.cronapp.io/eventos/'");
-    //        }
-         
-           
-       
-        
-    // }
+ 
 ]);
 
 
@@ -79,33 +58,13 @@ bot.dialog("mainMenu", [
         builder.Prompts.choice(session, "Para obter ajuda digite '@Cronappinho help' ou digite seu comando caso já saiba:", menuItems);
     },
     function(session, results){
-        if(results.response === "help"){
+        if(results.response){
             session.beginDialog(menuItems[results.response.entity].item);
             session.endDialog();
-        }else if(results.response === "templates"){
-            session.beginDialog(menuItems[results.response.entity].item);
-            session.endDialog();
-        }
-        else if(results.response === "horario-funcionamento"){
-            session.beginDialog(menuItems[results.response.entity].item);
-            session.endDialog();
-        }else if(results.response === "comercial"){
-            session.beginDialog(menuItems[results.response.entity].item);
-            session.endDialog();
-        }
-        else if(results.response === "youtube"){
-            session.beginDialog(menuItems[results.response.entity].item);
-            session.endDialog();   
-        }
-        else if(results.response === "webnars"){
-            session.beginDialog(menuItems[results.response.entity].item);
-            session.endDialog();     
         }
     }
 ])
 .triggerAction({
-    // The user can request this at any time.
-    // Once triggered, it clears the stack and prompts the main menu again.
     matches: /^main menu$/i,
     confirmPrompt: "deseja sair?"
 });
