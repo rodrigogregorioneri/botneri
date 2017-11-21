cfba80a1a087bfe6d92be323b8f770dc71ee123b
Rodrigo Neri <rodrigogregorioneri@hotmail.com>
2017-11-21 14:30:47 -0200

    Update app.js

--- app.js
+++ app.js
@@ -16,11 +16,24 @@ var connector = new builder.ChatConnector({
 
 
 var menuItems = { 
-    "futuro": {
-        item: "futuro"
+    "help": {
+        item: "help"
     },
-    "produtividade": {
-        item: "produtividade"
+    "templates": {
+        item: "templates"
+    },
+    "horario-funcionamento": {
+        item: "horario-funcionamento"
+    },
+    "comercial": {
+        item: "comercial"
+    }
+    ,
+    "youtube": {
+        item: "youtube"
+    },
+    "webnars": {
+        item: "webnars"
     },
 }
 
@@ -29,79 +42,107 @@ server.post('/api/messages', connector.listen());
 
 // This is a reservation bot that has a menu of offerings.
 var bot = new builder.UniversalBot(connector, [
-    function (session, results) {
-        session.send('"Bem vindo ao Suporte CronApp. Em breve você terá autonomia para registrar seus chamados em nosso portal cronapp.io/suporte"., nosso horário de atendimento é de Seg. a Sex. das 9:00 às 18:00. Aguarde, um de nossos analistas responderá em breve.');
-        session.send(':)');
-        session.send('Parabéns Tor filho de Odin, original de Asgard irmão de Loki.');
-        
-        builder.Prompts.text(session, "CronApp ou Notepad++?");
-    },
-    function (session, results) {
-        var resposta = results.response;
-   
-        if(resposta == "CronApp"){
-      
-            session.send("Sensacional assim você ganhara mais produtividade!!!");
-            
-        }else if(resposta == "Notepad++"){
-     
-            session.send("Você acaba de ganhar o titulo de Garoto Notepad++");  
+    function (session) {
           
-        }else{
-            session.send("Ops, Algo de errado não está certo!!!");  
-        }
-    
-            builder.Prompts.number(session, "qual sua idade?");
+       builder.Prompt.text(session, "Olá CronApp Users para obter ajuda digite '@Cronappinho help' ou digite seu comando caso já saiba:"); 
        
-        
+    //  session.send("Olá CronApp Users"); 
+      
     },
-    function (session, results) {
-        if(results.response >= 23){
-            session.send("Tiozão você!!");  
-            
-            
-        }else{
-            session.send("Menino novo!!");  
-            
-            
-        }
+    function(session, results){
         session.beginDialog("mainMenu");
     }
+ 
 ]);
 
 
 bot.dialog("mainMenu", [
     function(session){
-        builder.Prompts.choice(session, "Menu CronApp:", menuItems);
+        builder.Prompts.choice(session, "Para obter ajuda digite '@Cronappinho help' ou digite seu comando caso já saiba:", menuItems);
     },
     function(session, results){
-        if(results.response){
+        if(results.response == "help"){
             session.beginDialog(menuItems[results.response.entity].item);
-            session.endDialog();
+            //session.endDialog();
         }
     }
 ])
 .triggerAction({
-    // The user can request this at any time.
-    // Once triggered, it clears the stack and prompts the main menu again.
     matches: /^main menu$/i,
     confirmPrompt: "deseja sair?"
 });
 
 
-bot.dialog('futuro', [
-    function(session){
-        session.send("O futuro é o Blockly!");
-        
-
+bot.dialog('help', 
+    // Step 1
+    function (session) {
+        session.send("Comandos de Ajuda  \n  - Ajuda : help  \n  - Horario de funcionamento: horario-funcionamento  \n  - Templates de aberturas de chamados: templates  \n  - Contato comercial: comercial  \n  - Canal do Youtube: youtube  \n  - Webnars: webnar");
     }
-]);
+);
+
+bot.dialog('templates', 
+// Step 1
+function (session) {
+    session.send("  Projeto(s):  \n  Ambiente(s):  \n  Função do erro:  \n  \n  Passos:  \n  \n  OBS.:");
+}
+);
 
 
-bot.dialog('produtividade', [
-    function(session){
-        session.send("Alta produtividade com o blockly!!!");
-        
+bot.dialog('horario-funcionamento', 
+// Step 1
+function (session) {
+    session.send("Horário de atendimento é de Seg. a Sex. das 9:00 às 18:00.");    
+}
+);
 
+bot.dialog('comercial', 
+// Step 1
+function (session) {
+    session.send("  Comercial  \n  Representante: Gabriela Saeger  \n  Skype: Gabriela Saeger  \n  Telefone: +55 00 99999-9999");
+}
+);
+
+
+bot.dialog('youtube', 
+// Step 1
+function (session) {
+    ession.send("Siga nosso canal no Youtube e fique por dentro dos nossos tutoriais e as gravações dos nossos Webnars  \n  youtube: 'https://www.youtube.com/channel/UCfEBjfXaiA8n-YfezFklsfg'");
+}
+);
+
+bot.dialog('webnars', 
+// Step 1
+function (session) {
+    session.send("Cadastre-se nos webnars do CronApp: 'https://www.cronapp.io/eventos/'");
+}
+);
+
+
+
+bot.on('conversationUpdate', function (message) {
+    if (message.membersAdded && message.membersAdded.length > 0) {
+        var membersAdded = message.membersAdded
+            .map(function (m) {
+                var isSelf = m.id === message.address.bot.id;
+                return (isSelf ? message.address.bot.name : m.name) || '' + ' (Id: ' + m.id + ')';
+            })
+            .join(', ');
+
+        bot.send(new builder.Message()
+            .address(message.address)
+            .text('Bem vindo ao Suporte CronApp!!! Em breve você terá autonomia para registrar seus chamados em nosso portal "cronapp.io/suporte", nosso horário de atendimento é de Seg. a Sex. das 9:00 às 18:00. Aguarde, um de nossos analistas responderá em breve.'));
     }
-]);
+
+    if (message.membersRemoved && message.membersRemoved.length > 0) {
+        var membersRemoved = message.membersRemoved
+            .map(function (m) {
+                var isSelf = m.id === message.address.bot.id;
+                return (isSelf ? message.address.bot.name : m.name) || '' + ' (Id: ' + m.id + ')';
+            })
+            .join(', ');
+
+        bot.send(new builder.Message()
+            .address(message.address)
+            .text("Esperamos que você retorne ao CronApp!!!"));
+    }
+});
