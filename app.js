f7f6963a37f368d70d9da1d45f5c8ca7adf07089
Rodrigo Neri <rodrigogregorioneri@hotmail.com>
2017-11-29 17:51:39 -0200

    Update app.js

--- app.js
+++ app.js
@@ -32,9 +32,6 @@ var menuItems = {
     "webinars": {
         item: "webinars"
     },
-    "suporte": {
-        item: "suporte"
-    },
 }
 
 // Listen for messages from users 
@@ -58,7 +55,7 @@ bot.dialog("mainMenu", [
        // builder.Prompts.choice(session, "Para obter ajuda digite '@Help CronApp' ou digite seu comando caso já saiba:", menuItems);
 
 
-        builder.Prompts.choice(session, "Para obter ajuda digite '@Help CronApp' ou digite seu comando caso já saiba ou escolha no menu abaixo:", "documentação|horario-funcionamento|comercial|youtube|webinars|artigos|suporte", {
+        builder.Prompts.choice(session, "Para obter ajuda digite '@@Help CronApp' ou digite seu comando caso já saiba:", "documentação|horario-funcionamento|comercial|youtube|webinars|artigos", {
             retryPrompt: "Escolha invalida, insira novamente.",
             listStyle: builder.ListStyle.button,
             maxRetries: 2
@@ -85,9 +82,6 @@ bot.dialog("mainMenu", [
             case 'artigos':
                 session.beginDialog("artigos");
                 break;       
-             case 'suporte':
-                session.beginDialog("suporte");
-                break;      
         }
     }
 ])
@@ -147,15 +141,6 @@ function (session) {
     session.endDialog();
 }
 );
-
-bot.dialog('suporte', 
-// Step 1
-function (session) {
-    session.send("Bem vindo ao Suporte CronApp!!! \n Em breve você terá autonomia para registrar seus chamados em nosso portal "https://www.cronapp.io/suporte", nosso horário de \n atendimento é de Seg. a Sex. das 9:00 às 18:00. Aguarde, um de nossos analistas \n responderá em breve.");
-    session.endDialog();
-}
-);
-
 
 
 
